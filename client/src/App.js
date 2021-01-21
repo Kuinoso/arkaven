6b10e4f28d19ec4a786c59318cb3d6e62d6f0ea7
Kuinoso <cifuentes.k.l@gmail.com>
2021-01-21 10:09:15 -0300

    fix/2048_game_deleted

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import { Route } from 'react-router-dom';
 import MemoryGame from './components/memoryGame/Main';
 import SnakeGame from './components/snakeGame/Main';
 import TetrisGame from './components/tetrisGame/Main';
-import Game2048 from './components/Game2048/Main'; 
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Route exact path="/memoryGame" render={() => <MemoryGame />} />
       <Route exact path="/snakeGame" render={() => <SnakeGame />} />
       <Route exact path="/tetrisGame" render={() => <TetrisGame />} />
-      <Route exact path="/game2048" render={() => <Game2048 />} />
     </Router>
   );
 }
