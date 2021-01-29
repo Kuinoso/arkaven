43d9f0905ad416468a05c578652cf935ff2a0f9c
Kuinoso <cifuentes.k.l@gmail.com>
2021-01-29 17:13:01 -0300

    fix/style_bug

--- client/src/components/tetrisGame/Main/styles.js
+++ client/src/components/tetrisGame/Main/styles.js
@@ -1,10 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-    wrapper: {
-        width: '100vw',
-        height: '100vh',
-    },
     container: {
         display: 'flex',
         alignItems: 'flex-start',
