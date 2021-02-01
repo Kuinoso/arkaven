6fd163650b420e5f3e4bbbe48284cf22049859f5
Kuinoso <cifuentes.k.l@gmail.com>
2021-02-01 17:47:56 -0300

    feat/navbar_dinamic_buttons

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../images/logo.png';
 
@@ -11,24 +11,50 @@ import { useStyles } from './styles.js';
 
 export default function Navbar() {
     const classes = useStyles();
+    const location = useLocation();
+
+    const userButtons = () => {
+        return (
+            <div className={classes.userButtons}>
+                <Button color="inherit" className={classes.navButton}>Login</Button>
+                <Button color="inherit" className={classes.navButton}>Sign Up</Button>
+            </div>
+        );
+    };
+
+    const gameButtons = () => {
+        const params = ['tetrisGame', 'memoryGame', 'snakeGame', '2048Game'];
+
+        for (let i = 0; i < params.length; i++) {
+            if (location.pathname.includes(params[i])) {
+                const filteredParams = params.filter(item => item !== params[i]);
+
+                return (
+                    <div className={classes.gameButtons}>
+                        {filteredParams.map((item, i) =>
+                            <Link to={item} key={i}>
+                                <Button color="inherit" className={classes.navButton}>{item.split('G')[0]}</Button>
+                            </Link>
+                        )}
+                    </div>
+                );
+            };
+        };
+    };
 
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar className={classes.toolbar}>
-                <Link to='/'><img src={logo} alt='Logo' className={classes.logo} /></Link>
-                    <div className={classes.rigthNav}>
-                        <div className={classes.gameButtons}>
-                            <Link to='/tetrisGame'><Button color="inherit" className={classes.navButton}>Tetris</Button></Link>
-                            <Link to='memoryGame'><Button color="inherit" className={classes.navButton}>Memory</Button></Link>
-                            <Link to='/snakeGame'><Button color="inherit" className={classes.navButton}>Snake</Button></Link>
-                            <Link to='/2048Game'><Button color="inherit" className={classes.navButton}>2048</Button></Link>
+                    <Link to='/'><img src={logo} alt='Logo' className={classes.logo} /></Link>
+                    {location.pathname === '/' ?
+                        userButtons()
+                        :
+                        <div className={classes.rigthNav}>
+                            {gameButtons()}
+                            {userButtons()}
                         </div>
-                        <div className={classes.userButtons}>
-                            <Button color="inherit" className={classes.navButton}>Login</Button>
-                            <Button color="inherit" className={classes.navButton}>Sign Up</Button>
-                        </div>
-                    </div>
+                    }
                 </Toolbar>
             </AppBar>
         </div>
