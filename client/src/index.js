4c049b4617c260b77e824cb69fe3b5bfc7ed875d
Kuinoso <cifuentes.k.l@gmail.com>
2021-02-23 11:44:45 -0300

    fix/url_bug

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -12,7 +13,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+      <HashRouter>
         <App />
+      </HashRouter>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
